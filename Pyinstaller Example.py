3fd1bbc4e69aca415f73ac108ea75bad48808b2e
Jeremy Goldstein <jgoldstein@minlib.net>
2021-07-30 08:44:39 -0400

    reticulating splines

---
+++ Pyinstaller Example.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+"""Create and email a list of new items
+
+Author: Gem Stone-Logan
+Contact Info: gem.stone-logan@mountainview.gov or gemstonelogan@gmail.com
+"""
+
+import psycopg2
+import csv
+import requests
+import os
+import configparser
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
+def listQueries():
+	
+    file_list = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f)) and f.endswith('.sql')]
+    print(file_list)
+
+def runQuery(query,csvFile):
+
+    # import configuration file containing our connection string
+    # app.ini looks like the following
+    #[db]
+    #connection_string = dbname='iii' user='PUT_USERNAME_HERE' host='sierra-db.library-name.org' password='PUT_PASSWORD_HERE' port=1032
+
+    config = configparser.ConfigParser()
+    config.read('api_info.ini')
+    
+    conn = psycopg2.connect("dbname='iii' user='" + config['api']['sql_user'] + "' host='" + config['api']['sql_host'] + "' port='1032' password='" + config['api']['sql_pass'] + "' sslmode='require'")
+
+    #Opening a session and querying the database for weekly new items
+    cursor = conn.cursor()
+    
+    cursor.execute(open(query,"r").read())
+    #For now, just storing the data in a variable. We'll use it later.
+    columns = [i[0] for i in cursor.description]
+    rows = cursor.fetchall()
+    conn.close()
+    
+    '''
+    #use instead for Python 3.x
+    with open(csvFile,'w', encoding='utf-8', newline='') as tempFile:
+    '''
+    with open(csvFile,'wb') as tempFile:
+        myFile = csv.writer(tempFile, delimiter=',')
+        myFile.writerow(columns)
+        myFile.writerows(rows)
+    tempFile.close()
+    
+    return csvFile
+
+def main():
+    print('\nWhich query do you wish to run?\n')
+    #listQueries()
+    #print('\n')
+    Tk().withdraw()
+    my_query = askopenfilename(initialdir = "Queries/",title = "Select query",filetypes=[("Query Files", "*.sql")])
+    #use input instead of raw_input for Python 3.x
+    my_file_name = raw_input('\nEnter a name for your output file and click enter:\n') + '.csv'
+    path = 'Output/'
+    csvFile = os.path.join(path,my_file_name)
+    query_results = runQuery(my_query,csvFile)
+    print('Your file has been created in the Output folder')
+
+main()
